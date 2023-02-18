e066103e6455a027cfe6d50ff895e51b3c38e5fb
toneDrei79 <tone0709@icloud.com>
2023-02-18 13:41:52 +0100

    clean up

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 import * as THREE from "three";
-import { PlaneGeometry } from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GUI } from "three/addons/libs/lil-gui.module.min.js";
 import Stats from 'three/addons/libs/stats.module.js';
-const histogramVertexShader = load('./shaders/histogramvert.glsl')
-const histogramFragmentShader = load('./shaders/histogramfrag.glsl')
 const particlVertexShader = load('./shaders/particlevert.glsl')
 const particlFragmentShader = load('./shaders/particlefrag.glsl')
 
@@ -145,101 +142,48 @@ function render() {
             }
         }
     }
-    // var tmp = new THREE.PlaneGeometry(1, 1, video.videoWidth, video.videoHeight)
     var tmp = new THREE.BufferGeometry()
     tmp.setAttribute(
         'position',
         new THREE.BufferAttribute(new Float32Array(positions), 3)
-        // new THREE.BufferAttribute(positions, 3)
     )
     tmp.computeBoundingSphere()
-    // var points = new THREE.Mesh(new THREE.PlaneGeometry(1,1,video.videoWidth, video.videoHeight), bucketMat)
-
 
     if (points) {
         tmpScene.remove(points);
         points.geometry.dispose();
     }
     points = new THREE.Points(tmp, bucketMat)
-    // points = new THREE.Points(new PlaneGeometry(1, 1, video.videoHeight, video.videoWidth), bucketMat)
     tmpScene.add(points)
 
     const clearColor = renderer.getClearColor(new THREE.Color());
     renderer.setClearColor(new THREE.Color(0,0,0));
-
-    // renderer.clear();
     renderer.setClearColor(0x000000);
     renderer.setRenderTarget(dat)
     renderer.clear();
     for (let i=0; i<3; i++) {
         var mask = [0, 0, 0]
         mask[i] = 1
-        // console.log(mask)
         bucketMat.uniforms.color.value = mask
-        // renderer.setClearColor(0x000000);
-        // renderer.setRenderTarget(dat)
-        // renderer.clear();
         renderer.render(tmpScene, tmpCam)
-
-        // renderer.setRenderTarget(null)
     }
     renderer.setRenderTarget(null)
 
 
     renderer.setClearColor(clearColor);
 
-
-
-    let mem = new Float32Array(1*256*4)
-    renderer.readRenderTargetPixels(dat, 0, 0, 256, 1, mem)
-    // console.log(dat)
-    console.log(mem)
-    let r = new Float32Array(256)
-    let g = new Float32Array(256)
-    let b = new Float32Array(256)
-    for (let i=0; i<256; i++){
-        r[i] = mem[i*4]
-        g[i] = mem[i*4+1]
-        b[i] = mem[i*4+2]
-    }
-    // console.log(b)
-    // renderer.clear();
-
-    // const maxim = Math.max(...mem)
-    // console.log('a ' + maxim)
-
-    // var a = 0
-    // const hisme = new Float32Array(256)
-    // for (let i=0; i<256*4; i+=4) {
-    //     hisme[i/4] = mem[i]/maxim
-    //     // if (mem[i] == maxim) {
-    //     //     // console.log('aaa')
-    //     //     hisme[i/4] = 0
-    //     // }
-    //     a += mem[i]
-    // }
-    // console.log(a)
-    // const againmaxim = Math.max(...hisme)
-    // console.log('b ' + againmaxim)
-
-
-    // console.log(hisme)
-
+    // let mem = new Float32Array(1*256*4)
+    // renderer.readRenderTargetPixels(dat, 0, 0, 256, 1, mem)
+    // // console.log(dat)
+    // console.log(mem)
 
     histMat = new THREE.ShaderMaterial({
         vertexShader: drawv,
         fragmentShader: drawf,
         uniforms: {
-            hist: {type: 't', value: dat.texture}, // ???
-            // histmem: {type: 'v3v', value: hisme},
-            // hist: {type: 't', value: dat}, // ???
+            hist: {type: 't', value: dat.texture}
         },
     })
-    // histMat.needsUpdate = true
-    // var hhh = new THREE.PlaneGeometry(1, 1, 256, 5000)
-    // var hhmesh = new THREE.Mesh(hhh, histMat)
-    // hhmesh.scale.set(.4, .4, .4)
-    // scene2.add(hhmesh)
 
     if (histmesh) histmesh.material = histMat
 
@@ -256,71 +200,7 @@ function render() {
 
 function shad() {
     bucketMat.uniforms.tex.value = videoTexture
-    // bucketMat = new THREE.ShaderMaterial({
-    //     vertexShader: histv,
-    //     fragmentShader: histf,
-    //     uniforms: {
-    //         tex: {type: 't', value: videoTexture},
-    //     },
-    //     blending: THREE.CustomBlending,
-    //     blendEquation: THREE.AddEquation,
-    //     blendSrc: THREE.OneFactor,
-    //     blendDst: THREE.OneFactor,
-    //     // side: THREE.DoubleSide
-    // })
-
-    // const positions = new Float32Array(video.videoHeight*video.videoWidth*3).fill(0)
-    /*
-    var positions = []
-    for (let j=0; j<video.videoHeight; j++) {
-        for (let i=0; i<video.videoWidth; i++) {
-            positions.push(i/video.videoWidth, j/video.videoHeight, 0.0)
-        }
-    }
-    // var tmp = new THREE.PlaneGeometry(1, 1, video.videoWidth, video.videoHeight)
-    var tmp = new THREE.BufferGeometry()
-    tmp.setAttribute(
-        'position',
-        new THREE.BufferAttribute(new Float32Array(positions), 3)
-        // new THREE.BufferAttribute(positions, 3)
-    )
-    tmp.computeBoundingSphere()
-    // var points = new THREE.Mesh(new THREE.PlaneGeometry(1,1,video.videoWidth, video.videoHeight), bucketMat)
-
-
-    if (points) {
-        tmpScene.remove(points);
-        points.geometry.dispose();
-    }
-    points = new THREE.Points(tmp, bucketMat)
-    // points = new THREE.Points(new PlaneGeometry(1, 1, video.videoHeight, video.videoWidth), bucketMat)
-    tmpScene.add(points)
-    */
-    // points.position.set(-0.9, 0.3, 0.0)
-    // points.scale.set(.2, .2, .2)
-    // scene2.add(points) // visualize histgram bucket data
-
-
-    // renderer.setClearColor(0x000000);
-    // renderer.setRenderTarget(dat)
-    // renderer.clear();
-    // renderer.render(tmpScene, tmpCam)
-    // let mem = new Float32Array(1*256*4)
-    // renderer.readRenderTargetPixels(dat, 0, 0, 256, 1, mem)
-    // // console.log(dat)
-    // console.log(mem)
-
-    // renderer.setRenderTarget(null)
-
-
-    // histMat = new THREE.ShaderMaterial({
-    //     vertexShader: drawv,
-    //     fragmentShader: drawf,
-    //     uniforms: {
-    //         tex: {type: 't', value: dat.texture}, // ???
-    //     },
-    // })
-
+    
     var hhh = new THREE.PlaneGeometry(1, 1, 256, 5000)
     var hhmesh = new THREE.Mesh(hhh, histMat)
 
@@ -378,69 +258,6 @@ function animate() {
 }
 
 
-function generateHistgram () {
-    const canvas = document.createElement( 'canvas' );
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-
-    const context = canvas.getContext( '2d' );
-    context.drawImage( videoTexture.image, 0, 0 );
-
-    const data = context.getImageData( 0, 0, canvas.width, canvas.height ).data;
-    // console.log( data.length );
-
-    var rgb = [];
-    var skip = 32;
-    for (let i=0; i<data.length; i+=4*skip) {
-        rgb.push([data[i], data[i+1], data[i+2]])
-    }
-    // console.log(Math.max(...rgb))
-
-    const histR = Array(256).fill(0)
-    const histG = Array(256).fill(0)
-    const histB = Array(256).fill(0)
-    for (let _rgb of rgb) {
-        histR[_rgb[0]]++
-        histG[_rgb[1]]++
-        histB[_rgb[2]]++
-    }
-    const max = Math.max(...histR.concat(histG).concat(histB))
-    // console.log(max)
-    for (let i=0; i<256; i++) {
-        histR[i] /= max
-        histG[i] /= max
-        histB[i] /= max
-    }
-    // console.log(histR)
-
-
-    const uni = {
-        r: { type: 'v3v', value: histR},
-        g: { type: 'v3v', value: histG},
-        b: { type: 'v3v', value: histB},
-        tex: { type: 't', value: videoTexture},
-        w: video.videoWidth,
-        h: video.videoHeight,
-        hist: { type: 'v3v', value: Array(video.videoWidth*video.videoHeight)}
-    };
-    
-    var material = new THREE.ShaderMaterial({
-        uniforms: uni,
-        // vertexShader: document.getElementById("vertexShader").textContent,
-        // fragmentShader: document.getElementById("fragmentShader").textContent,
-        vertexShader: histogramVertexShader,
-        fragmentShader: histogramFragmentShader,
-    });
-
-    var geometry = new THREE.PlaneGeometry(1.5, 1, 256, max);
-
-    var mesh = new THREE.Mesh(geometry, material);
-    mesh.name = 'graph'
-    mesh.material.side = THREE.DoubleSide;
-
-    return mesh
-}
-
 function textureToPoint(texture, width, height) {
     var discret = 2;
 
@@ -491,7 +308,7 @@ function onWindowResize() {
 
 
 
-function load(url, data){
+function load(url){
     var request = new XMLHttpRequest();
     request.open('GET', url, false);
     request.send(null);
@@ -503,23 +320,6 @@ function load(url, data){
 }
 
 
-
-function createShader(gl, sourceCode, type) {
-    // Compiles either a shader of type gl.VERTEX_SHADER or gl.FRAGMENT_SHADER
-    const shader = gl.createShader(type);
-    gl.shaderSource(shader, sourceCode);
-    gl.compileShader(shader);
-
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-        const info = gl.getShaderInfoLog(shader);
-        throw `Could not compile WebGL program. \n\n${info}`;
-    }
-    return shader;
-}
-
-var toType = function(obj) {
-    return ({}).toString.call(obj).match(/\s([a-zA-Z]+)/)[1]
-}
 
 function generateHistogramSpace() {
     const space = new THREE.Group()
