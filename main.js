3ff3d7d6f0a8b5d36a8716a153a0ad4f31468689
toneDrei79 <tone0709@icloud.com>
2023-03-08 13:46:01 +0100

    random

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import * as THREE from 'three'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import { GUI } from 'three/addons/libs/lil-gui.module.min.js'
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js'
+import GUI from 'three/addons/libs/lil-gui.module.min.js'
 import Stats from 'three/addons/libs/stats.module.js'
 import Histogram from './histogram.js'
 import Particle from './particle.js'
